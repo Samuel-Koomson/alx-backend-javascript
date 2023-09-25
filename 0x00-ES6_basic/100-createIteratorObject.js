3551b4caeda0f5cd9d700daf6155d7785b05c938
Samuel <kskoomson1@gmail.com>
2023-09-25 23:45:21 +0000

    ES6 basics for JavaScript

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+    const employees = [];
+    for (const [dept, employee] of Object.entries(report.allEmployees)) {
+      for (const employ of employee) {
+        employees.push(employ);
+      }
+    }
+  
+    return employees;
+  }
